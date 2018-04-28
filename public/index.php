c2434664a856dc119d632849321a35ea5c549d23
Berkay Aydin <sbayd06@gmail.com>
2018-04-28 09:28:33 +0300

    Some style update

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
       <!-- Basic Page Needs
          –––––––––––––––––––––––––––––––––––––––––––––––––– -->
       <meta charset="utf-8">
-      <title>Shitty Log Easily Logs Lazy Developers</title>
+      <title>Simple and Fast Logging mechanism for Lazy Developers</title>
       <meta name="description" content="Easy Logs for Lazy Developers">
       <meta name="author" content="Berkay Aydin">
       <!-- Mobile Specific Metas
@@ -21,6 +21,11 @@
       <!-- Favicon
          –––––––––––––––––––––––––––––––––––––––––––––––––– -->
       <link rel="icon" type="image/png" href="images/favicon.png">
+      <style type="text/css">
+        .overflowShit {
+          word-break: break-word;
+        }
+      </style>
    </head>
    <body>
       <!-- Primary Page Layout
@@ -29,25 +34,27 @@
          <div class="row">
             <div class="eleven column" style="margin-top: 5%">
                <h4>Hey Lazy Developer!</h4>
-               <p>ShittyLog is a very simple log mechanism! You just need to make a http request to your link. After that you can see the log file as text! </p>
+               <p>ShittyLog is a very simple log mechanism! You just need to make a http request to your link. After that you can see the log file as text!</p>
+               <p>You can also use the javascript code <a href="#jsCodeBlockP">below</a></p>
                <p>These links are special for you! Don't worry about it. Also all logs will be deleted weekly.</p>
                <table class="u-full-width">
                   <tbody>
                      <tr>
-                        <td style="min-width: %25;">Your Log Endpoint</td>
-                        <td><a target="_blank" href="<?php echo(USER_ENDPOINT."&logData=sampleLogData"); ?>"><?php echo(USER_ENDPOINT."&logData=sampleLogData"); ?></a></td>
+                        <td>Your Log Endpoint</td>
+                        <td ><a target="_blank" href="<?php echo(USER_ENDPOINT."&logData=sampleLogData"); ?>"><?php echo(USER_ENDPOINT."&logData=sampleLogData"); ?></a></td>
                      </tr>
                      <tr>
-                        <td style="width: %25;">Your Log Link</td>
-                        <td><a target="_blank" href="<?php echo(USER_LOG_FILE); ?>"><?php echo(USER_LOG_FILE); ?></a></td>
+                        <td>View Your Logs</td>
+                        <td class="overflowShit"><a target="_blank" href="<?php echo(USER_LOG_FILE); ?>"><?php echo(USER_LOG_FILE); ?></a></td>
                      </tr>
                      <tr>
-                        <td style="min-width: %25;">Delete Existing Log Data</td>
-                        <td><a target="_blank" href="<?php echo(USER_ENDPOINT."&logData=sampleLogData&del"); ?>"><?php echo(USER_ENDPOINT."&logData=deletedAllLogData&del"); ?></a></td>
+                        <td>Data Deletion</td>
+                        <td><a target="_blank" href="<?php echo(USER_ENDPOINT."&logData=sampleLogData&del"); ?>">Click here to all existing logs!</a></td>
                      </tr>
                   </tbody>
                </table>
-               <p>I have created a simple javascript request function for your log purposes! You are free to use it!</p>
+               <p id="jsCodeBlockP">I have created a simple javascript request function for your log purposes! You are free to use it!</p>
+               <p>It is working on all browsers and JS environments!</p>
                <p>Please copy this function to your project </p>
                <pre><code>
 function shittyLog() {
@@ -77,14 +84,22 @@ function shittyLog() {
                <pre><code>
   shittyLog('sample', { json: 'data', example: 3});
 </code></pre>
-               <p>After that; View it from;     
-                  <a href="<?php echo(USER_LOG_FILE); ?>"><?php echo(USER_LOG_FILE); ?></a>
+               <p>After that; You can view your logs     
+                  <a href="<?php echo(USER_LOG_FILE); ?>">here!</a>
             </div>
          </div>
-               <p>All files located publicly on <a href="https://github.com/sbayd/shittylog/" target="_blank">GitHub</a></p>
-               <p>Created by Berkay Aydin, <a href="https://sbaydin.com" target="_blank">https://sbaydin.com</a></p>
+         <p>All files located publicly on <a href="https://github.com/sbayd/shittylog/" target="_blank">GitHub</a></p>
+         <p>Created by Berkay Aydin, <a href="https://sbaydin.com" target="_blank">https://sbaydin.com</a></p>
       </div>
       <!-- End Document
          –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <a
+        href="https://github.com/sbayd/shittylog/"
+        title="View it on GitHub"
+        target="_blank">
+        <img
+          style="width:50px; position:absolute; top: 3%; right: 20px"
+          src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"/>
+        </a>
    </body>
 </html>
